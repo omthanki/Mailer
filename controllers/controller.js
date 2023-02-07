d5a50c71a4fa4b7999f39ff68e92caead6ee0f71
Om Thanki <othanki874@rku.ac.in>
2023-02-07 19:26:11 +0530

    Last commit

--- controllers/controller.js
+++ controllers/controller.js
@@ -41,38 +41,47 @@ module.exports.home = async (req, res) => {
 
 module.exports.sendMail = async (req, res) => {
 
+    if (req.method == "GET"){
+        res.render("sendmail")
+    }
 
-    try {
+    if (req.method == "POST") {
 
-        const emailAdd = req.body.useremail
+        try {
 
-        let transporter = nodemailer.createTransport({
-            host: "smtp.gmail.com",
-            port: 465,
-            secure: true,
-            auth: {
-                user: process.env.EMAIL,
-                pass: process.env.PASSWORD,
-            },
-        });
+            const data = await emailModel.find({}, { _id: 0, __v: 0 })
 
-        let message = {
-            from: process.env.EMAIL, // sender address
-            to: emailAdd, // list of receivers
-            subject: "Node Mailer", // Subject line
-            text: "Hello", // plain text body
-            html: "<b>Hello world?</b>", // html body
+            var a=[]
+            
+            data.map((Email) => {a.push(Email.email);})
+
+            let transporter = nodemailer.createTransport({
+                host: "smtp.gmail.com",
+                port: 465,
+                secure: true,
+                auth: {
+                    user: process.env.EMAIL,
+                    pass: process.env.PASSWORD,
+                },
+            });
+
+            let message = {
+                from: process.env.EMAIL, // sender address
+                to: a, // list of receivers
+                subject: "Node Mailer", // Subject line
+                text: "Hello", // plain text body
+            }
+
+            const isSent = await transporter.sendMail(message)
+
+            if (isSent) {
+                res.send("Sent successfully")
+            }
+
+        } catch (error) {
+            console.log(error);
+            res.send(error)
         }
-
-        const data = await transporter.sendMail(message)
-
-        if (data) {
-            res.send("Sent successfully")
-        }
-
-    } catch (error) {
-        console.log(error);
-        res.send(error)
     }
 
 }
--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,7 @@ const controller = require("../controllers/controller")
 
 routes.post("/", controller.home)
 routes.get("/", controller.home)
+routes.get("/sendmail", controller.sendMail)
 routes.post("/sendmail", controller.sendMail)
 
 module.exports = routes
